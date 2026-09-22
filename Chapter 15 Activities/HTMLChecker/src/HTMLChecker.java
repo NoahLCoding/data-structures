63b6218d5097103040e111c460437e2b8a2fa8ff
NoahLCoding <noahnojus2008@gmail.com>
2026-09-22 15:03:26 -0500

    Update HTMLChecker.java

--- Chapter 15 Activities/HTMLChecker/src/HTMLChecker.java
+++ Chapter 15 Activities/HTMLChecker/src/HTMLChecker.java
@@ -17,14 +17,42 @@ public class HTMLChecker
 {
     public static void main(String[] args)
     {
-        String filename = "src/TagSample1.html";
-
+        String filename = "C:\\Users\\nnlatakas\\Desktop\\Software Engineering\\data-structures\\Chapter 15 Activities\\HTMLChecker\\src\\TagSample2.html";
+        Stack<String> htmlTags = new Stack<>();
+        
         try (Scanner in = new Scanner(new File(filename)))
         {
-            // Your code goes here
-            . . .
+            String tags = "";
+            while (in.hasNext()){
+                String tag = in.next();
+                tags += tag + " ";
 
+            }
+            
+            String[] tagArray = tags.split(" ");
+            for (int i = 0; i < tagArray.length; i++){
+                String tag = tagArray[i];
+                if (tag.startsWith("<") && !tag.startsWith("</")){
+                    htmlTags.push(tag);
+                } 
+                else if (tag.startsWith("</")){
+                    if (htmlTags.isEmpty()){
+                        System.out.println("Error: There is a closing tag without an opening tag.");
+                        return;
+                    }
+                    String openingTag = htmlTags.pop();
+                    if (!openingTag.substring(1).equals(tag.substring(2))){
+                        System.out.println("Error: The closing tag " + tag + " does not match the opening tag " + openingTag);
+                        return;
+                    }
+                }
+            }
 
+            if (htmlTags.isEmpty()){
+                System.out.println("All tags are properly nested.");
+            } else {
+                System.out.println("Error: There are unclosed tags.");
+            }
         } catch (FileNotFoundException e)
         {
             System.out.println("Cannot open: " + filename);
